99f02093963f05471f2bb51778b8e9527ed1235a
Aminu Mohammed <thestunnaman1@gmail.com>
2022-08-22 17:01:05 +0000

    more updates

--- prob_and_stats/script.js
+++ prob_and_stats/script.js
@@ -8,7 +8,7 @@ function submit(){
        });
     return datasetArr;
     } else {
-       alert("Invalid data submitted. Enter numbers separated by commas");
+       alert("Invalid data submitted. Ensure you entered numbers separated by commas(,)");
     };  
 }
 
@@ -21,58 +21,74 @@ function submitWeight(){
 }
 
 function sortedArr(){
-    const sortedArr = submit().sort((a,b) => a - b);
-    const quantity = sortedArr.length;
-    const displayDataSet = document.getElementById("data").innerHTML = "[Dataset: " + sortedArr + "]";
-    const displayWeights = document.getElementById("display-weights").innerHTML ="[Weights: " + submitWeight() + "]";
-    const displayFrequency = document.getElementById("display-frequency").innerHTML ="[Frequency: " + quantity + "]";
-    return document.getElementById("data").innerHTML = displayDataSet, displayWeights, displayFrequency;
+    if(submit() !== 0 && submit().length>1){
+        const sortedArr = submit().sort((a,b) => a - b);
+        const quantity = sortedArr.length;
+        const displayDataSet = document.getElementById("data").innerHTML = "[Dataset: " + sortedArr + "]";
+        const displayWeights = document.getElementById("display-weights").innerHTML ="[Weights: " + submitWeight() + "]";
+        const displayFrequency = document.getElementById("display-frequency").innerHTML ="[Dataset Count: " + quantity + "]";
+        console.log(sortedArr);
+        return document.getElementById("data").innerHTML = displayDataSet, displayWeights, displayFrequency;
+    } else {
+        alert("Enter a valid dataset first");
+    }
 }
 
 function findMean(){
-    const mean = submit().reduce((a,b)=> a+b, 0) / submit().length;
-    return document.getElementById("mean").value = mean; 
+    if(submit() !== 0 && submit().length>1){
+        const mean = submit().reduce((a,b)=> a+b, 0) / submit().length;
+        return document.getElementById("mean").value = mean;
+    } else {
+        alert("Enter a valid dataset first");
+    } 
 }
 
 function findWeightedMean(){
-    const values = submit();
-    const weightData = submitWeight();
-    if(values.length !== weightData.length){
-        alert("Values and Weights are of unequal length. Kindly reenter")
-    } else {
-        let a = 0;
-        let b = 0;
-        for (let i=0; i < values.length; ++i){
-            const weight = weightData[i];
-            a += weight * values[i];
-            b += weight;
+    if(submit() !== 0 && submit().length>1){
+        const values = submit();
+        const weightData = submitWeight();
+        if(values.length !== weightData.length){
+            alert("Values and Weights are of unequal length. Kindly reenter")
+        } else {
+            let a = 0;
+            let b = 0;
+            for (let i=0; i < values.length; ++i){
+                const weight = weightData[i];
+                a += weight * values[i];
+                b += weight;
+            }
+            const weightedMean = a / b;
+            return document.getElementById("weighted-mean").value = weightedMean;
         }
-        const weightedMean = a / b;
-        return document.getElementById("weighted-mean").value = weightedMean;
-    }
-
+    } else {
+    alert("Enter a valid dataset first");
+    } 
 }
 
 function findMode(){
-    const sortedArr = submit().sort((a,b) => a - b);
-    const mode = {};
-    let max = 0, count = 0;
+    if(submit() !== 0 && submit().length>1){
+        const sortedArr = submit().sort((a,b) => a - b);
+        const mode = {};
+        let max = 0, count = 0;
 
-    for(let i=0; i < sortedArr.length; i++){
-        const item = sortedArr[i];
+        for(let i=0; i < sortedArr.length; i++){
+            const item = sortedArr[i];
 
-        if(mode[item]){
-            mode[item]++;
-        } else {
-            mode[item] = 1;
+            if(mode[item]){
+                mode[item]++;
+            } else {
+                mode[item] = 1;
+            }
+
+            if(count < mode[item]){
+                max = item;
+                count = mode[item];
+            }
         }
-
-        if(count < mode[item]){
-            max = item;
-            count = mode[item];
-        }
-    }
-    return document.getElementById("mode").value = max;
+        return document.getElementById("mode").value = max;
+    } else {
+        alert("Enter a valid dataset first");
+    } 
 }
 
 function findMedian(){
@@ -89,83 +105,108 @@ function findMedian(){
 }
 
 function findRange(){
-    const data = submit().sort((a,b) => a - b);
-    const max = data[data.length - 1];
-    const min = data[0];
-    const range = max - min;
-    return document.getElementById("range").value = range;
+    if(submit() !== 0 && submit().length>1){   
+        const data = submit().sort((a,b) => a - b);
+        const max = data[data.length - 1];
+        const min = data[0];
+        const range = max - min;
+        return document.getElementById("range").value = range;
+    } else {
+        alert("Enter a valid dataset first");
+    } 
 }
 
 function findMeanDeviation(){
-    const data = submit();
-    const length = data.length;
-    const mean = submit().reduce((a,b)=> a+b, 0) / submit().length;
-    const arr = data.map(function(element){
-        return Math.abs(element - mean);
-    });
-    const md = arr.reduce((a,b)=> a+b, 0)/length;
-    return document.getElementById("mean-deviation").value = md;
+    if(submit() !== 0 && submit().length>1){
+        const data = submit();
+        const length = data.length;
+        const mean = submit().reduce((a,b)=> a+b, 0) / submit().length;
+        const arr = data.map(function(element){
+            return Math.abs(element - mean);
+        });
+        const md = arr.reduce((a,b)=> a+b, 0)/length;
+        return document.getElementById("mean-deviation").value = md;
+    } else {
+        alert("Enter a valid dataset first");
+    } 
 }
 
 function findVariance(){
-    const data = submit();
-    const length = data.length;
-    const x = data.reduce((a,b)=> a+b, 0);
-    const x_sq = data.map(function(element){
-        return Math.abs(Math.pow(element,2));
-    });
-    const v = x_sq.reduce((a,b)=> a+b, 0);
-    const variance = (v/length) - Math.pow((x/length),2);
-    return document.getElementById("variance").value = variance;
+    if(submit() !== 0 && submit().length>1){
+        const data = submit();
+        const length = data.length;
+        const x = data.reduce((a,b)=> a+b, 0);
+        const x_sq = data.map(function(element){
+            return Math.abs(Math.pow(element,2));
+        });
+        const v = x_sq.reduce((a,b)=> a+b, 0);
+        const variance = (v/length) - Math.pow((x/length),2);
+        return document.getElementById("variance").value = variance;
+    } else {
+        alert("Enter a valid dataset first");
+    } 
 }
 
 function findStandardDeviation(){
-    const data = submit();
-    const length = data.length;
-    const x = data.reduce((a,b)=> a+b, 0);
-    const x_sq = data.map(function(element){
-        return Math.abs(Math.pow(element,2));
-    });
-    const v = x_sq.reduce((a,b)=> a+b, 0);
-    const variance = (v/length) - Math.pow((x/length),2);
+    if(submit() !== 0 && submit().length>1){
+        const data = submit();
+        const length = data.length;
+        const x = data.reduce((a,b)=> a+b, 0);
+        const x_sq = data.map(function(element){
+            return Math.abs(Math.pow(element,2));
+        });
+        const v = x_sq.reduce((a,b)=> a+b, 0);
+        const variance = (v/length) - Math.pow((x/length),2);
 
-    const sd = Math.sqrt(variance);
-    document.getElementById("standard-deviation").value = sd;
+        const sd = Math.sqrt(variance);
+        document.getElementById("standard-deviation").value = sd;
+    } else {
+        alert("Enter a valid dataset first");
+    } 
 }
 
 function findQuartileDeviation(){
-    const sortedData = submit().sort((a,b) => a - b);
-    const half = Math.ceil(sortedData.length / 2);
-    const firstHalf = sortedData.slice(0, half);
-    const secondHalf = sortedData.slice(half);
+    if(submit() !== 0 && submit().length>1){
+        const sortedData = submit().sort((a,b) => a - b);
+        const half = Math.ceil(sortedData.length / 2);
+        const firstHalf = sortedData.slice(0, half);
+        const secondHalf = sortedData.slice(half);
 
-    function findMedian(arr){
-        const sortedData = arr.sort((a,b) => a - b);
-        if(sortedData.length % 2 !== 0){
-            const oddMedian = sortedData[Math.floor(sortedData.length/2)];
-            return oddMedian;
-        } else {
-            let mid1 = sortedData[sortedData.length / 2];
-            let mid2 = sortedData[sortedData.length / 2 - 1]
-            const evenMedian = (mid1 + mid2) / 2;
-            return evenMedian; 
+        function findMedian(arr){
+            const sortedData = arr.sort((a,b) => a - b);
+            if(sortedData.length % 2 !== 0){
+                const oddMedian = sortedData[Math.floor(sortedData.length/2)];
+                return oddMedian;
+            } else {
+                let mid1 = sortedData[sortedData.length / 2];
+                let mid2 = sortedData[sortedData.length / 2 - 1]
+                const evenMedian = (mid1 + mid2) / 2;
+                return evenMedian; 
+            }
         }
-    }
-   return document.getElementById("quartile-deviation").value = ((findMedian(secondHalf) - findMedian(firstHalf)) / 2);
+    return document.getElementById("quartile-deviation").value = ((findMedian(secondHalf) - findMedian(firstHalf)) / 2);
+    } else {
+    alert("Enter a valid dataset first");
+    } 
 }
 
 function findCoVariance(){
-    const data = submit();
-    const length = data.length;
-    const x = data.reduce((a,b)=> a+b, 0);
-    const x_sq = data.map(function(element){
-        return Math.abs(Math.pow(element,2));
-    });
-    const v = x_sq.reduce((a,b)=> a+b, 0);
-    const variance = (v/length) - Math.pow((x/length),2);
-    const sd = Math.sqrt(variance);
-    const mean = data.reduce((a,b)=> a+b, 0) / data.length;
+    if(submit() !== 0 && submit().length>1){
+        const mean = submit().reduce((a,b)=> a+b, 0) / submit().length;
 
-    const cv = Math.floor(((sd/mean) * 1) * 100);
-    return document.getElementById("cf-variance").value = cv + "%";
+        const data = submit();
+        const length = data.length;
+        const x = data.reduce((a,b)=> a+b, 0);
+        const x_sq = data.map(function(element){
+            return Math.abs(Math.pow(element,2));
+        });
+        const v = x_sq.reduce((a,b)=> a+b, 0);
+        const variance = (v/length) - Math.pow((x/length),2);
+        const sd = Math.sqrt(variance);
+
+        const cv = Math.floor(((sd/mean) * 1) * 100);
+        return document.getElementById("cf-variance").value = cv + "%";
+    } else {
+        alert("Enter a valid dataset first");
+    } 
 }
